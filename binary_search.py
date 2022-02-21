a64c50341a778f77bd40eef1689d3a4cb9a9807f
chargeshivers <varghese.chris@gmail.com>
2022-02-21 11:36:17 -0600

    new file:   binary_search.py 	new file:   quicksort.py

---
+++ binary_search.py
@@ -0,0 +1,9 @@
+def binary_search(A, x):
+    def bs(i,j):
+        if i>j:
+            return False
+        if i==j:
+            return A[i] == x
+        mid = (i+j)//2
+        return bs(i,mid) if x <= A[mid] else bs(mid+1,j)
+    return bs(0,len(A)-1)
