572b08cfe9ee8e55d8fcc1310a1d1aeffcd4022b
chargeshivers <varghese.chris@gmail.com>
2022-03-11 20:57:30 -0600

    new file:   lowest_common_ancestor.py

---
+++ lowest_common_ancestor.py
@@ -0,0 +1,22 @@
+"""
+https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+def lowestCommonAncestor(root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    def search(r):
+        if r in [None, p, q]:
+            return r
+        left, right = search(r.left) , search(r.right)
+        if left and right:
+            return r
+        else:
+            return left or right
+    return search(root)
